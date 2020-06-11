313b0ff0d2a2760d77f6245f4001fb97ca36e4fa
Rodrigo Bighetti <robighetti@gmail.com>
2020-06-11 17:47:17 -0300

    configuração do jest e criação dos testes unitarios

---
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,84 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUserService.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    await createUserService.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@gmail.com',
+        password: '111111',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,51 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+import CreateUserService from './CreateUserService';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserRepository = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUserRepository.execute({
+      name: 'Rodrigo Bighetti',
+      email: 'robighetti@gmail.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with exists email', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserRepository = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    await createUserRepository.execute({
+      name: 'Rodrigo Bighetti',
+      email: 'robighetti@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUserRepository.execute({
+        name: 'Rodrigo Bighetti',
+        email: 'robighetti@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});

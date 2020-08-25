47e6af97e77541e179cc88dd3742c32ea83b3deb
Rodrigo Bighetti <robighetti@gmail.com>
2020-08-25 11:58:45 -0300

    ajustes no service do ListProviderMonthAvailability

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -51,7 +51,7 @@ class ListProviderMonthAvailabilityService {
       return {
         day,
         available:
-          isAfter(new Date(), compareDate) && appointmentsInDay.length < 10,
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
